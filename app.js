73542a082c02e4552dff64a50b00905c536776da
Yash Jha <jhayash99@gmail.com>
2023-04-10 01:09:44 +0530

    added cors

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const imageRouter = require("./routes/image")
 const productRouter = require("./routes/product")
 const wishlistRouter = require("./routes/wishlist")
 const adminRouter = require("./routes/admin");
+const cors = require('cors')
 const { admin, auth, access, user } = require('./middlewares/auth');
 
 var app = express();
@@ -21,7 +22,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', "ejs");
 
-app.use(logger("tiny"));
+app.use(cors())
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
