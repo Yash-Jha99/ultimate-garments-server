ba45f750125d97b78542ba17aeecd4b1755238ce
Yash Jha <105184445+Yash-Jha99@users.noreply.github.com>
2023-04-12 01:56:46 +0530

    Update db.js

--- middlewares/db.js
+++ middlewares/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql')
 var db = mysql.createConnection({
-    host: MYSQL_HOST,
-    port: MYSQL_PORT,
-    user: MYSQL_USER,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
 })
 
 db.connect((error, res) => {
