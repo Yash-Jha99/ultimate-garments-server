d3d65dbdee875bf337728a6c4e6f3dea721fbfd2
Yash Jha <jhayash99@gmail.com>
2023-04-25 01:44:09 +0530

    mysql

--- middlewares/db.js
+++ middlewares/db.js
@@ -3,4 +3,11 @@ require("dotenv").config();
 
 var db = mysql.createConnection(process.env.MYSQL_URI);
 
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  } else console.log("Connected to MySQL");
+});
+
 module.exports = db;
